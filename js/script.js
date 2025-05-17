5b887bdb5926da8cc26b2ecaacc5e7a004e7ea12
Mera-Pakistan <wasiqnaveed123@gmail.com>
2025-05-17 23:37:46 +0500

    first javascript event created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 
+const submitBtn = document.querySelector(".submit-btn");
+
+const headingOne = document.querySelector("h1");
+
+function randombgChange(Red , green , blue){
+
+    headingOne.style.background = `rgb(${Red} , ${green} , ${blue})`;
+}
+
+submitBtn.addEventListener("click" , () =>{
+
+    r = Math.floor(Math.random() * 256);
+
+    g = Math.floor(Math.random() * 256);
+
+    b = Math.floor(Math.random() * 256);
+
+    randombgChange(r , g , b);
+
+});
